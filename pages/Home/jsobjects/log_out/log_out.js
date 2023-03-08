fdc1faf1e0f61eeffaa8beeebc9e9eab8f632736
kittithat.sin <kittithat.sin@mfec.co.th>
2023-03-08 20:17:15 +0000

    f

--- pages/Home/jsobjects/log_out/log_out.js
+++ pages/Home/jsobjects/log_out/log_out.js
@@ -1,6 +1,7 @@
 export default {
 	Logout: () => {
 		Object.keys(appsmith.store).map(key =>storeValue(key, undefined));
+			showAlert('ออกจากระบบสำเร็จ.', 'success')
 	//	clearStore()
 			navigateTo('Login')
 	}
